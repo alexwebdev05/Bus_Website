0d5e9c4a8171490dac84e0208d0a0c69900a1ac9
alexwebdev05 <alexwebdev05@gmail.com>
2024-06-02 14:37:21 +0000

    Map first steps

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -12,29 +12,85 @@ function Map() {
     // Renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xECDFFF)
+    renderer.setClearColor(0x17202A)
 
     // Camera
     const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 130;
+    camera.position.z = 5;
+    camera.position.x = -2;
+    camera.position.y = 5;
 
     // Scene
     const scene = new THREE.Scene();
 
     // Light
-    const intensity = 1;
+    const intensity = 5;
     const light = new THREE.DirectionalLight(0xffffff, intensity);
-    light.position.set(-2, 4, -2);
+    light.position.set(2, 4, 2);
     scene.add(light);
 
     // Object loader
     const loader = new GLTFLoader();
+    let map, bus, signal
+    const signals = []
+
     loader.load('/map1.glb', function(gltf) {
-      scene.add(gltf.scene);
+      map = gltf.scene;
+      scene.add(map);
       render();
     }, undefined, function(error) {
       console.error(error);
     });
+
+    loader.load('/bus.glb', function(gltf) {
+      bus = gltf.scene;
+      bus.position.set(0.4, 0.61, 2.35);
+      bus.rotation.y = THREE.MathUtils.degToRad(90)
+      scene.add(bus);
+      render();
+    }, undefined, function(error) {
+      console.error(error);
+    });
+
+    // loader.load('/signal.glb', function(gltf) {
+    //   signal = gltf.scene;
+    //   signal.position.set(0.4, 1.1, 2.05);
+    //   signal.scale.set(0.5, 0.5, 0.5)
+    //   scene.add(signal);
+    //   render();
+    // }, undefined, function(error) {
+    //   console.error(error);
+    // });
+
+    function addSignal(position, scale) {
+      loader.load('/signal.glb', function(gltf) {
+          let signal = gltf.scene;
+          signal.traverse(function(child) {
+            if (child.isMesh) {
+                child.material = new THREE.MeshBasicMaterial({ color: child.material.color });
+            }
+          });
+
+          signal.position.set(position.x, position.y, position.z);
+          signal.scale.set(scale.x, scale.y, scale.z);
+          scene.add(signal);
+          signals.push(signal); // Añadir la instancia de signal al array
+          render();
+  
+          // Iniciar la animación si es la primera signal añadida
+          if (signals.length === 1) {
+              animate();
+          }
+      }, undefined, function(error) {
+          console.error(error);
+      });
+    }
+
+    addSignal(new THREE.Vector3(0.4, 1.1, 2.05), new THREE.Vector3(0.5, 0.5, 0.5));
+    addSignal(new THREE.Vector3(1.44, 1.2, -1.08), new THREE.Vector3(0.5, 0.5, 0.5));
+    addSignal(new THREE.Vector3(-1.35, 1.2, -2.6), new THREE.Vector3(0.5, 0.5, 0.5));
+    addSignal(new THREE.Vector3(-2.6, 1.2, -0.6), new THREE.Vector3(0.5, 0.5, 0.5));
+    addSignal(new THREE.Vector3(0.3, 1.2, -0.38), new THREE.Vector3(0.5, 0.5, 0.5));
 
     // OrbitControls
     const controls = new OrbitControls(camera, renderer.domElement);
@@ -56,8 +112,14 @@ function Map() {
 
     // Animation loop
     const render = (time) => {
-      time *= 0.001; // convert time to seconds
-      controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+      time *= 0.001;
+      controls.update();
+      const targetPosition = new THREE.Vector3();
+      camera.getWorldPosition(targetPosition);
+
+      signals.forEach(signal => {
+          signal.lookAt(targetPosition);
+      });
       renderer.render(scene, camera);
       requestAnimationFrame(render);
     };
