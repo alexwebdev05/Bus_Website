0d2df3dd0ebe5c7920f24cffb3ea04b6f7944136
alexwebdev05 <alexfriasangulo@gmail.com@>
2024-09-09 22:14:14 +0200

    makeing button

---
+++ clientnext/app/ui/homeButton/hombeButton.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import style from "./style.module.css"
+
+interface buttonProps {
+    text: string;
+    url: string;
+    backgroundColor: string;
+}
+
+export default function HomeButton({text,url, backgroundColor}: buttonProps) {
+    return (
+        <Link
+            className={style.container}
+            href={url}
+            style={{"--main": `var(${backgroundColor})`} as React.CSSProperties}
+        >
+            {text}
+        </Link>
+    )
+}
