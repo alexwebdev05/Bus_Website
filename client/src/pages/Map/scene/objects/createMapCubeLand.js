ca1ea44cec57ca01aab268801f454fe6428c064a
alexwebdev05 <alexfriasangulo@gmail.com@>
2024-09-10 19:45:39 +0200

    Migration finished

--- client/src/pages/Map/scene/objects/createMapCubeLand.js
+++
@@ -1,8 +0,0 @@
-export function createMapCubeLand(loader, scene) {
-    loader.load('models/map1.glb', function(gltf) {
-        const map = gltf.scene;
-        scene.add(map);
-    }, undefined, function(error) {
-        console.error(error);
-    });
-}
--- client/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-import Home from './Home/Home.jsx'
-import Map from './Map/Map.jsx'
-
-export default { Home, Map }
