acf0c1111848847725a2eb500a682ef7d873d921
alexwebdev05 <alexfriasangulo@gmail.com>
2024-09-10 11:45:45 +0200

    Buttons  styles fixed

--- clientnext/app/ui/button/Button.tsx
+++ clientnext/app/ui/button/Button.tsx
@@ -2,24 +2,18 @@ import Link from "next/link";
 import style from "./style.module.css";
 
 interface ButtonProps {
-    url?: string;
+    url: string;
     text: string;
     backgroundColor: string;   
 }
 
 export default function Button({url, text, backgroundColor}: ButtonProps) {
-
-    const content = (
-        <button className={style.button} style={{'--background-color': `var(${backgroundColor})`} as React.CSSProperties }>
+    return (
+        <Link
+            className={style.button} style={{'--background-color': `var(${backgroundColor})`} as React.CSSProperties }
+            href={url}
+        >
             {text}
-        </button>
-    )
-
-    return url ? (
-        <Link href={url}>
-            {content}
         </Link>
-    ) : (
-        content
-    );
+    )
 }
