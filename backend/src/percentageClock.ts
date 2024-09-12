8e25b5ccacb5a259dcfdeb7b4e1b0638beca4b23
alexwebdev05 <alexfriasangulo@gmail.com@>
2024-09-12 19:32:56 +0200

    clock bug fixed

--- backend/src/percentageClock.ts
+++ backend/src/percentageClock.ts
@@ -1,50 +1,50 @@
 import WebSocket from "ws";
 
 export function percentageClock() {
-        
     // Percentage
-    const TIME = 120
-    const startTime = new Date().getTime();
-    const stopTime = startTime + TIME * 1000;
-    var percentage: number;
+    const TIME = 120;
+    let startTime = new Date().getTime();
+    let stopTime = startTime + TIME * 1000;
+    let percentage: number;
 
-    var wss = new WebSocket.Server({ port: 3002 })
+    const wss = new WebSocket.Server({ port: 3002 });
 
     wss.on('connection', (ws) => {
         console.log('Connected percentage clock');
-    
-        // Send the initial percentage when a new client connects
+
         ws.send(JSON.stringify({ percentage: getPercentage() }));
-    
+
         // When a client disconnects
         ws.on('close', () => {
-        console.log('Client disconnected');
+            console.log('Client disconnected');
         });
     });
 
     const intervalId = setInterval(() => {
         percentage = getPercentage();
-    
-        // Broadcast the percentage to all connected clients
+
         wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ percentage }));
-        }
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ percentage }));
+            }
         });
-    
-        // Stop the interval if time is up
+
         if (percentage <= 0) {
-        clearInterval(intervalId);
+            clearInterval(intervalId);
+        } else if (percentage >= 1) {
+            startTime += TIME * 1000;
+            stopTime += TIME * 1000;
         }
     }, 10);
 
     function getPercentage() {
         let actualTime = new Date().getTime();
         let leftover = stopTime - actualTime;
-    
+
         let percentage = 1 - leftover / (TIME * 1000);
-        return Math.min(Math.max(percentage, 0), 100);
+        console.log(percentage);
+        return Math.min(Math.max(percentage, 0), 1);
     }
 
-    console.log('WebSocket server is running on ws://localhost:3001');
+    console.log('WebSocket server is running on ws://localhost:3002');
 }
